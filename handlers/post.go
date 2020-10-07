756abb9469444564d5abe0b5d59d873fd0802f4a
深蓝 <mail@ri-co.cn>
2020-10-07 16:22:34 +0800

    一个小bug

--- handlers/post.go
+++ handlers/post.go
@@ -61,7 +61,8 @@ func UploadPics(ctx iris.Context) {
 	}
 	defer out.Close()
 	io.Copy(out, file)
-	whole := "https://api.ri-co.cn/whisper/getpics/" + fname
+	//上传地址，本地测试or服务器
+	whole := "http://localhost:8090/whisper/getpics/" + fname
 	ctx.WriteString(whole)
 	fmt.Println("上传图片成功", fname)
 }
--- main.go
+++ main.go
@@ -15,44 +15,47 @@ func main() {
 		AllowCredentials: true,
 	})
 	whisper := app.Party("/whisper", crs).AllowMethods(iris.MethodOptions)
+	{
 
-	whisper.Post("/login", handlers.Login)
-	whisper.Post("/regist", handlers.Regist)
-	whisper.Get("/logout", handlers.Logout)
+		whisper.Post("/login", handlers.Login)
+		whisper.Post("/regist", handlers.Regist)
+		whisper.Get("/logout", handlers.Logout)
 
-	whisper.Get("/getUserInfoByName", handlers.GetUserInfoByName)
-	whisper.Get("/getUserInfoByCookie", handlers.GetUserInfoByCookie)
+		whisper.Get("/getUserInfoByName", handlers.GetUserInfoByName)
+		whisper.Get("/getUserInfoByCookie", handlers.GetUserInfoByCookie)
 
-	whisper.Get("/getTags", handlers.GetTags)
-	whisper.Get("/getGroupNames", handlers.GetGroupNames)
-	whisper.Get("/getGroups", handlers.GetGroups)
-	whisper.Get("/getPostByGroup", handlers.GetPostByGroup)
+		whisper.Get("/getTags", handlers.GetTags)
+		whisper.Get("/getGroupNames", handlers.GetGroupNames)
+		whisper.Get("/getGroups", handlers.GetGroups)
+		whisper.Get("/getPostByGroup", handlers.GetPostByGroup)
 
-	whisper.Get("/getAllPost", handlers.GetAllPost)
-	whisper.Get("/getLazyPost", handlers.GetLazyPost)
-	whisper.Get("/getPostByUser", handlers.GetPostByUser)
+		whisper.Get("/getAllPost", handlers.GetAllPost)
+		whisper.Get("/getLazyPost", handlers.GetLazyPost)
+		whisper.Get("/getPostByUser", handlers.GetPostByUser)
 
-	whisper.Get("/getAllReply", handlers.GetAllReply)
-	whisper.Get("/getReplys", handlers.GetReplys)
-	whisper.Get("/readMsg", handlers.ReadMsg)
+		whisper.Get("/getAllReply", handlers.GetAllReply)
+		whisper.Get("/getReplys", handlers.GetReplys)
+		whisper.Get("/readMsg", handlers.ReadMsg)
 
-	whisper.Get("/changeAvatar", handlers.ChangeAvatar)
-	whisper.Get("/changeBannar", handlers.ChangeBannar)
-	whisper.Post("/changeInfo", handlers.ChangeInfo)
+		whisper.Get("/changeAvatar", handlers.ChangeAvatar)
+		whisper.Get("/changeBannar", handlers.ChangeBannar)
+		whisper.Post("/changeInfo", handlers.ChangeInfo)
 
-	whisper.Get("/delPost", handlers.Admin, handlers.DelPost)
-	whisper.Get("/delGroupOnly", handlers.Admin, handlers.DelGroupOnly)
-	whisper.Get("/delGroupAll", handlers.Admin, handlers.DelGroupAll)
-	whisper.Get("/delUserByPost", handlers.Admin, handlers.DelUserByPost)
-	whisper.Get("/delMyPost", handlers.DelMyPost)
-	whisper.Get("/delReply", handlers.DelReply)
+		whisper.Get("/delPost", handlers.Admin, handlers.DelPost)
+		whisper.Get("/delGroupOnly", handlers.Admin, handlers.DelGroupOnly)
+		whisper.Get("/delGroupAll", handlers.Admin, handlers.DelGroupAll)
+		whisper.Get("/delUserByPost", handlers.Admin, handlers.DelUserByPost)
+		whisper.Get("/delMyPost", handlers.DelMyPost)
+		whisper.Get("/delReply", handlers.DelReply)
 
-	whisper.Post("/uploadPics", handlers.UploadPics)
-	whisper.Post("/newPost", handlers.NewPost)
-	whisper.Post("/newGroup", handlers.NewGroup)
-	whisper.Post("/newReply", handlers.NewReply)
+		whisper.Post("/uploadPics", handlers.UploadPics)
+		whisper.Post("/newPost", handlers.NewPost)
+		whisper.Post("/newGroup", handlers.NewGroup)
+		whisper.Post("/newReply", handlers.NewReply)
 
-	whisper.HandleDir("/getpics", iris.Dir("./uploadpics"))
+		whisper.HandleDir("/getpics", iris.Dir("./uploadpics"))
+
+	}
 	app.Run(iris.Addr(":8090"))
 
 	return
